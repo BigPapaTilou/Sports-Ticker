e3435c4b4362f56a8bca3afdf33dca47e3bfd8e8
BigPapaTilou <matteoportais@gmail.com>
2026-07-22 19:10:03 +0200

    Update filter-test.js
    Debug game dates

--- js/filter-test.js
+++ js/filter-test.js
@@ -1,7 +1,37 @@
+// Sports Ticker Filters - Debug
+
+
 function filterRelevantGames(games){
 
-    alert("FILTER CHARGE");
 
-    return [];
+    console.log(
+        "TOTAL GAMES :",
+        games.length
+    );
+
+
+
+    games.forEach(game => {
+
+
+        console.log(
+
+            game.league,
+
+            game.home?.name,
+
+            game.date,
+
+            game.raw?.date
+
+        );
+
+
+    });
+
+
+
+    return games;
+
 
 }
