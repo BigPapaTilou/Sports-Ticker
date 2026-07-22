95d0a5979eef78f9b3afd6a7a56e65a92f98c500
BigPapaTilou <matteoportais@gmail.com>
2026-07-22 19:21:12 +0200

    Update filters.js
    Restore real filter

--- js/filters.js
+++ js/filters.js
@@ -1,10 +1,76 @@
-// Sports Ticker Filters TEST
+// Sports Ticker Filters
 
 
 function filterRelevantGames(games){
 
-    console.log("FILTER.JS LOADED");
 
-    return [];
+    const now = new Date();
+
+
+    const limit = new Date(
+        now.getTime() + 48 * 60 * 60 * 1000
+    );
+
+
+
+    return games.filter(game => {
+
+
+        const gameDate = new Date(
+            game.raw?.date
+        );
+
+
+
+        const state =
+        game.raw
+        ?.competitions?.[0]
+        ?.status
+        ?.type
+        ?.state;
+
+
+
+        // Match en direct
+
+        if(state === "in"){
+
+            return true;
+
+        }
+
+
+
+        // Match à venir dans les 48h
+
+        if(state === "pre"){
+
+            return (
+                gameDate >= now &&
+                gameDate <= limit
+            );
+
+        }
+
+
+
+        return false;
+
+
+    })
+
+
+    .sort((a,b)=>{
+
+
+        return (
+            new Date(a.raw.date)
+            -
+            new Date(b.raw.date)
+        );
+
+
+    });
+
 
 }
